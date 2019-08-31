145224459fa8b9c255b9b53340e8935d52f350a1
hwarr <hwarr@mail.ru>
2019-08-31 13:21:38 +0500

    Updates

---
+++ precache-manifest.21f5ce1111d815b3e8c05c3054831900.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592b3005a2f62dfddafe1664e5478729",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "9fac72f1e65fbf5a50cd",
+    "url": "/react-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5928327819fd5c7d6cf3",
+    "url": "/react-app/static/js/2.7c039cdd.chunk.js"
+  },
+  {
+    "revision": "9fac72f1e65fbf5a50cd",
+    "url": "/react-app/static/js/main.b9aac7c8.chunk.js"
+  },
+  {
+    "revision": "5889b9934aea6962d00e",
+    "url": "/react-app/static/js/runtime~main.c3506144.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
