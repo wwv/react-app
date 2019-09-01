380da0878aba6d3aafa51991a1fd3dfee39efd13
hwarr <hwarr@mail.ru>
2019-09-02 00:56:46 +0500

    Updates

--- precache-manifest.dabf25985b03def47d88a06f41e041ee.js
+++ precache-manifest.dabf25985b03def47d88a06f41e041ee.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "86b94e01c6a7e85848b103ff2296423b",
+    "revision": "65401ef663e64709b65116b12656fe3e",
     "url": "/react-app/index.html"
   },
   {
-    "revision": "416d8b986478c13c675a",
+    "revision": "6388ae35292c49fb4fb0",
     "url": "/react-app/static/css/main.34de6062.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-app/static/js/2.24d4b2d3.chunk.js"
   },
   {
-    "revision": "416d8b986478c13c675a",
-    "url": "/react-app/static/js/main.8635b113.chunk.js"
+    "revision": "6388ae35292c49fb4fb0",
+    "url": "/react-app/static/js/main.7c708e54.chunk.js"
   },
   {
     "revision": "5889b9934aea6962d00e",
