b582314ed5fca17d91159b423816b96b73cb6f93
hwarr <hwarr@mail.ru>
2019-09-01 15:53:03 +0500

    clear dummy data

--- src/components/user-list.js
+++ src/components/user-list.js
@@ -8,20 +8,6 @@ import Snackbar from '@material-ui/core/Snackbar';
 import { Link } from "react-router-dom";
 import { connect } from 'react-redux';
 import {initialState, USER_LIST_REQUEST} from "../sagas/UserListSaga";
-import {makeStyles} from "@material-ui/core";
-
-
-function createData(name, calories, fat, carbs, protein) {
-    return {name, calories, fat, carbs, protein};
-}
-
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
 
 class UserList extends React.Component {
     componentDidMount() {
@@ -50,7 +36,7 @@ class UserList extends React.Component {
                                       role="checkbox"
                                       tabIndex={-1}>
                                 <TableCell component="th" scope="row">
-                                    {row.id}
+                                    {row.userId}
                                 </TableCell>
                                 <TableCell align="right">{row.name}</TableCell>
                                 <TableCell align="right">{row.email}</TableCell>
